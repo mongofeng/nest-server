10ca7b64a1ec4ce626a043d6419f1398b5873358
yangpu <yangpu@xiao100.com>
2021-06-30 15:15:15 +0800

    feat: 添加file

--- src/file/upload/upload.controller.ts
+++ src/file/upload/upload.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('upload')
-export class UploadController {}
+export class UploadController {
+  @Post('upload')
+  @UseInterceptors(FilesInterceptor('files'))
+  uploadFile(@UploadedFiles() files: Array<Express.Multer.File>) {
+    console.log(files);
+  }
+}
