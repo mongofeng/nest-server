7370837c42d63f5ec5b4dc21b4ea6a404c7faf88
yangpu <yangpu@xiao100.com>
2021-06-30 17:23:16 +0800

    feat: 去除console

--- src/file/upload/upload.controller.ts
+++ src/file/upload/upload.controller.ts
@@ -13,14 +13,12 @@ export class UploadController {
   @Post('multi-upload')
   @UseInterceptors(FilesInterceptor('files'))
   uploadMutiFile(@UploadedFiles() files: Array<Express.Multer.File>) {
-    console.log(files);
     return R.ok(files);
   }
 
   @Post('image')
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
     return R.ok(file);
   }
 }
