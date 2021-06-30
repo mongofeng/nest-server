bade45ff9a398e858017b2faf69952dc62ca250f
yangpu <yangpu@xiao100.com>
2021-06-30 15:06:54 +0800

    feat: 添加nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UploadController } from './upload/upload.controller';
+import { FileModule } from './file/file.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UploadController],
+  imports: [FileModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
